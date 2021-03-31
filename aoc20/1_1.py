ba44731a9b96f8c8ad9194c034790ff992bad6dc
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2021-03-31 17:09:55 +0200

    Update 1_1.py

--- aoc20/1_1.py
+++ aoc20/1_1.py
@@ -9,4 +9,17 @@ inputA = [
 
 inputA = [int(x) for x in open('input1_1.txt').read().split()]
 
+def leseEingabeListe(zahlenListeDatei:str):
+    result = []
+    with open(zahlenListeDatei) as file:
+        inputText = file.read()
+        zeilenListe = inputText.split()
+        
+        for zeile in zeilenListe:
+            zahl = int(zeile)
+            result.append(zahl)
+
+    return result
+inputA = leseEingabeListe("input1_1.txt")
+
 [x*y*z for z in inputA for y in inputA for x in inputA if x+y+z==2020][0]
